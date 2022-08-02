b5f6f5b78883f1aa35262c553f41333eab550795
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-03 00:04:00 +0200

    commit

--- modules.js
+++ modules.js
@@ -1,7 +1,9 @@
 
 const names=require('./names');
 const sayHi=require('./utils');
+const data=require('./alternative-flavor');
 console.log(names);
 sayHi('Jess');
 sayHi(names.Am);
 sayHi(names.Gh);
+console.log(data);
