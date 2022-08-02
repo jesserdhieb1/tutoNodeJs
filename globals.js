8ac6b5c876c6046318bff3afc630f03edc167aa3
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-02 16:47:20 +0200

    commit

---
+++ globals.js
@@ -0,0 +1,8 @@
+console.log(__dirname);
+console.log(__filename);
+//console.log(process);
+//console.log(require);
+//console.log(module);
+setInterval(()=>{
+    console.log('wassssap :) !')
+},1000);
